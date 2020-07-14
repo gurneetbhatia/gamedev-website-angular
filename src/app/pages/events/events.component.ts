0e7bed690dbfc0754cbdf863f8a7bed9864872e1
Gurneet Bhatia <sbgurneet@gmail.com>
2020-07-14 17:24:06 +0400

    sorts events into upcoming and past

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -11,19 +11,36 @@ import { Event } from './event';
 export class EventsComponent implements OnInit {
 
   events: Event[];
+  upcomingEvents: Event[];
+  pastEvents: Event[];
 
   constructor(private dataService: DataService,
-              private notificationService: NotificationService) { }
+              private notificationService: NotificationService) {
+                this.upcomingEvents = [];
+                this.pastEvents = [];
+              }
 
   ngOnInit(): void {
     this.dataService.getAllEvents().subscribe(
       (data) => {
         this.events = data;
+        this.sortEvents();
       },
       (error) => {
         this.notificationService.showError("There was an error fetching the events, please try again later.")
       }
     )
+  }
+
+  sortEvents() {
+    const today = new Date();
+    this.events.forEach((event) => {
+      if (event.datetime >= today) {
+        this.upcomingEvents.push(event)
+      } else {
+        this.pastEvents.push(event);
+      }
+    })
   }
 
 }
